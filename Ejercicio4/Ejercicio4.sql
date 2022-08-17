8118ed94192099b381f6ad1e1ecf7006a0e99b45
jordi campos <jordicampos.97@gmail.com>
2022-08-17 14:06:22 +0200

    Ejercicio del 1 al 6 terminado

---
+++ Ejercicio4/Ejercicio4.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS mydb;
+CREATE DATABASE IF NOT EXISTS mydb;
+
+USE mydb;
+
+CREATE TABLE IF NOT EXISTS album (
+id_Album INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+album_Name VARCHAR(255) NOT NULL,
+dateRealeased DATETIME
+);
+CREATE TABLE IF NOT EXISTS genre (
+id_Genre INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+genre_Name VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Artist (
+id_Artist INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+artist_Name VARCHAR(255) NOT NULL
+);
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+INSERT INTO mydb.album(album_Name) VALUES ("JORDI");
+
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+INSERT INTO mydb.artist(artist_Name) VALUES ("JORDI");
+
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+INSERT INTO mydb.genre(genre_Name) VALUES ("JORDI");
+
+INSERT INTO mydb.album (album_Name) VALUES
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID"),
+("DAVID");
+
+
+
+SELECT * FROM mydb.album;
+SELECT * FROM mydb.artist;
